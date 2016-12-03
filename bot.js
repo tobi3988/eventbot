8688d7acaf1759a68b02bc3924d2e1fe5141fd4c
Tobias Bachmann <tobiasba@student.ethz.ch>
2016-12-03 14:15:44 +0100

    show next 3 events

--- bot.js
+++ bot.js
@@ -33,33 +33,42 @@ function getEventsAndReply(bot, message, date) {
         .auth(process.env.TSRI_USER, process.env.TSRI_PW)
         .end(function (err, res) {
 
-            if(res.body.objects.length > 0){
-                var reply_with_attachments = {
-                    'username': 'Eventbot',
-                    'text': 'Der naechste Event:',
-                    'attachments': [
-                        {
-                            'fallback': 'To be useful, I need you to invite me in a channel.',
-                            'title': res.body.objects[0].title,
-                            'text': res.body.objects[0].description,
-                            'color': '#7CD197'
-                        }
-                    ],
-                    'icon_url': res.body.objects[0].image
+            var events = res.body.objects;
+            if(events.length > 0){
+                bot.reply(message, "Ich habe " + events.length + " Events gefunden.");
+
+                for(var i=0; i<3 && events.length >i; i++) {
+
+                    var reply_with_attachments = {
+                        'username': 'Eventbot',
+                        'text': 'Der ' + (i + 1) +'. Event:',
+                        'attachments': [
+                            {
+                                'fallback': 'To be useful, I need you to invite me in a channel.',
+                                'title': events[i].title,
+                                'text': events[i].description,
+                                'color': '#7CD197'
+                            }
+                        ],
+                        'icon_url': events[i].image
+                    }
+                    bot.reply(message, reply_with_attachments);
                 }
-                bot.reply(message, reply_with_attachments);
             } else {
                 bot.reply(message, 'Sorry, heute läuft nichts');
             }
         });
 }
 
+function replyEventsToday(bot, message) {
+    var today = new Date().toISOString().slice(0, 10);
+    getEventsAndReply(bot, message, today);
+}
 controller.hears(
     ['heute', 'hüt'],
     ['direct_message', 'direct_mention'],
     function (bot, message) {
-        var today = new Date().toISOString().slice(0, 10);
-        getEventsAndReply(bot, message, today);
+        replyEventsToday(bot, message);
     }
 );
 
@@ -71,6 +80,14 @@ controller.hears(
         date.setDate(date.getDate() + 1);
         var tomorrow = date.toISOString().slice(0, 10);
         getEventsAndReply(bot, message, tomorrow);
+    }
+);
+
+controller.hears(
+    ['was l.*ft'],
+    ['ambient'],
+    function (bot, message) {
+        replyEventsToday(bot, message);
     }
 );
 
