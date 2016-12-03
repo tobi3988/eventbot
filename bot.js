8c51eea18c7b266ece258887529c0aa38aa08970
Tobias Bachmann <tobiasba@student.ethz.ch>
2016-12-03 15:13:29 +0100

    added categories

--- bot.js
+++ bot.js
@@ -28,9 +28,17 @@ controller.hears(
     }
 );
 
-function getEventsAndReply(bot, message, date) {
+function getEventsAndReply(bot, message, date, category) {
+    var queryparams ={};
+    if(category){
+        queryparams={date: date, category: category}
+    } else {
+        queryparams={date: date}
+    }
+
     request
         .get('https://tsri.ch/api/v0/agenda/?date=' + date + '')
+        .query(queryparams)
         .auth(process.env.TSRI_USER, process.env.TSRI_PW)
         .end(function (err, res) {
 
@@ -56,26 +64,40 @@ function getEventsAndReply(bot, message, date) {
                     bot.reply(message, reply_with_attachments);
                 }
             } else {
-                bot.reply(message, 'Sorry, heute läuft nichts');
+                bot.reply(message, 'Sorry, es läuft nichts');
             }
         });
 }
 
 function replyEventsToday(bot, message) {
     var today = new Date().toISOString().slice(0, 10);
-    getEventsAndReply(bot, message, today);
+    var category = getCategory(message.text);
+    getEventsAndReply(bot, message, today, category);
 }
 
 const days_of_the_week = [
   /(sunntig?|sonntag)/i,
-  /(mäntig?|montag)/i,
-  /(dienstag)/i,
-  /(mittwoch)/i,
-  /(donnschtig?|donnerstag)/i,
+  /(mänt(i|a)g?|montag)/i,
+  /(dienstag|zischt(i|a)g?)/i,
+  /(mittw.ch)/i,
+  /(d.nn?schtig?|donnerstag)/i,
   /(frit.g?|freitag)/i,
   /(samscht.g?|samstag)/i,
 ];
 
+function getCategory(text) {
+    var category = '';
+    if(/konzert/i.test(text)){
+        category = 'konzert'
+    }
+    if(/divers.*/i.test(text)){
+        category = 'diverses'
+    }
+    if(/kino/i.test(text)){
+        category = 'kino'
+    }
+    return category
+}
 controller.hears(
     ['.*'],
     ['direct_message', 'direct_mention'],
@@ -84,10 +106,11 @@ controller.hears(
             replyEventsToday(bot, message);
             return;
         } else if (/(morgen|morn)/i.test(message.text)) {
+            var category = getCategory(message.text);
             var date = new Date();
             date.setDate(date.getDate() + 1);
             var tomorrow = date.toISOString().slice(0, 10);
-            getEventsAndReply(bot, message, tomorrow);
+            getEventsAndReply(bot, message, tomorrow, '');
             return;
         } else {
             var now = moment();
@@ -95,7 +118,8 @@ controller.hears(
             for (var i=0; i<days_of_the_week.length; ++i) {
                 if (days_of_the_week[i].test(message.text)) {
                     now.day(7 + i);
-                    getEventsAndReply(bot, message, now.format('YYYY-MM-DD'));
+                    var category = getCategory(message.text);
+                    getEventsAndReply(bot, message, now.format('YYYY-MM-DD'), category);
                     return;
                 }
             }
