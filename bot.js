e63f4435204b8bfa671a46ab267ee4fd5f2da0d8
Tobias Bachmann <tobiasba@student.ethz.ch>
2016-12-03 13:56:05 +0100

    morn

--- bot.js
+++ bot.js
@@ -17,7 +17,7 @@ controller.hears(
     function (bot, message) {
         bot.api.users.info(
             {user: message.user},
-            function(error, response) {
+            function (error, response) {
                 console.log(response.user);
                 bot.reply(message, '@' + response.user.name + ' hey!');
             }
@@ -27,16 +27,50 @@ controller.hears(
     }
 );
 
+function getEventsAndReply(bot, message, date) {
+    request
+        .get('https://tsri.ch/api/v0/agenda/?date=' + date + '')
+        .auth(process.env.TSRI_USER, process.env.TSRI_PW)
+        .end(function (err, res) {
+
+            if(res.body.objects.length > 0){
+                var reply_with_attachments = {
+                    'username': 'Eventbot',
+                    'text': 'Der naechste Event:',
+                    'attachments': [
+                        {
+                            'fallback': 'To be useful, I need you to invite me in a channel.',
+                            'title': res.body.objects[0].title,
+                            'text': res.body.objects[0].description,
+                            'color': '#7CD197'
+                        }
+                    ],
+                    'icon_url': res.body.objects[0].image
+                }
+                bot.reply(message, reply_with_attachments);
+            } else {
+                bot.reply(message, 'Sorry, heute läuft nichts');
+            }
+        });
+}
+
 controller.hears(
-    ['heute'],
+    ['heute', 'hüt'],
     ['direct_message', 'direct_mention'],
     function (bot, message) {
-        console.log('heute');
-        request
-            .get('https://tsri.ch/api/v0/agenda/')
-            .auth(process.env.TSRI_USER, process.env.TSRI_PW)
-            .end(function(err, res){
-                console.log(res.body);
-            });
+        var today = new Date().toISOString().slice(0, 10);
+        getEventsAndReply(bot, message, today);
     }
 );
+
+controller.hears(
+    ['morgen', 'morn'],
+    ['direct_message', 'direct_mention'],
+    function (bot, message) {
+        var date = new Date();
+        date.setDate(date.getDate() + 1);
+        var tomorrow = date.toISOString().slice(0, 10);
+        getEventsAndReply(bot, message, tomorrow);
+    }
+);
+
